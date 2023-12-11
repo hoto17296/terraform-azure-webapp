b4d3b5f8f0e6fab4047e88925da3c0dcc3dc3fd6
Yuki Ishikawa <hoto@y32.jp>
2023-12-11 12:14:12 +0900

    add id-token test endpoint

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
-from fastapi import Depends, FastAPI
+import jwt
+from fastapi import Depends, FastAPI, Request, Response
 from models import AccessLog
 from sqlmodel import func, select
 from sqlmodel.ext.asyncio.session import AsyncSession
@@ -15,3 +16,12 @@ async def root(db: AsyncSession = Depends(get_db_session)):
     statement = select(func.count(AccessLog.id))
     count = (await db.exec(statement)).one()
     return {"count": count}
+
+
+@app.get("/me")
+async def me(request: Request):
+    id_token = request.headers.get("x-ms-token-aad-id-token")
+    if id_token is None:
+        return Response(status_code=401)
+    claims: dict = jwt.decode(id_token, options={"verify_signature": False})
+    return {"claims": claims}
