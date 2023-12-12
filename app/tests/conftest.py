1d7a0164004ea17c6ad50114401177ae8c754d9e
Yuki Ishikawa <hoto@y32.jp>
2023-12-13 00:02:29 +0900

    bugfix

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -8,15 +8,20 @@ dsn = "postgresql+asyncpg://postgres:deadbeef@database/test"
 engine = create_async_engine(dsn)
 AsyncSessionMaker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
+db_setup_statements = [
+    "DROP SCHEMA public CASCADE",
+    "CREATE SCHEMA public",
+]
+
+with open("/database/ddl.sql", "rt") as f:
+    for statement in f.read().split(";"):
+        if statement.strip():
+            db_setup_statements.append(statement.strip())
+
 
 @pytest.fixture
 async def db(scope="function"):
     async with AsyncSessionMaker() as session:
-        await session.exec(text("DROP SCHEMA public CASCADE"))
-        await session.exec(text("CREATE SCHEMA public"))
-        with open("/database/ddl.sql", "rt") as f:
-            for statement in f.read().split(";"):
-                statement = statement.strip()
-                if statement:
-                    await session.exec(text(statement))
+        for statement in db_setup_statements:
+            await session.exec(text(statement))
         yield session
